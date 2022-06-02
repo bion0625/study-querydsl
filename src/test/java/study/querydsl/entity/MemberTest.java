a01a213777947ddfe4c281fc2d2fafaeea38cf7b
이의정 <onairsparks@gmail.com>
2022-06-02 13:14:54 +0900

    오타수정

--- src/test/java/study/querydsl/entity/MemberTest.java
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -25,11 +25,11 @@ class MemberTest {
         em.persist(teamA);
         em.persist(teamB);
 
-        Member member1 = new Member("mamber1", 10, teamA);
-        Member member2 = new Member("mamber2", 20, teamA);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
 
-        Member member3 = new Member("mamber3", 30, teamB);
-        Member member4 = new Member("mamber4", 40, teamB);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
         em.persist(member1);
         em.persist(member2);
         em.persist(member3);
